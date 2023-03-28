3e3b1d0d28a2cce9daf54cc689d406c25b40a290
onikarubi <printuserselect@gmail.com>
2023-03-28 11:41:57 +0900

    Implementation messaging api bot.
    ラインボットの応答機能を実装

--- requests_test.py
+++ requests_test.py
@@ -18,8 +18,8 @@ def request_post_content(url):
             'y': 4,
         }
         response = requests.post(
-            url, json=data)
-        print(response.status_code, response.headers)
+            url, json=data, headers={'Content-Type': 'application/json'})
+        print(response.status_code, response.headers['Content-Type'])
 
     except:
         print('通信に失敗')
@@ -28,5 +28,6 @@ def request_post_content(url):
 if __name__ == '__main__':
     url1 = 'https://selfcare_prot-1-v6064451.deta.app/'
     url2 = 'http://127.0.0.1:8000'
+    url3 = 'https://sample-self-care-app.onrender.com'
 
     request_post_content(url1)

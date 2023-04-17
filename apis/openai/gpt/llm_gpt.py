effcef370db8724e6067a10b28bcd7f7627a60ec
onikarubi <printuserselect@gmail.com>
2023-04-17 09:17:16 +0900

    create chat completion.

--- apis/openai/gpt/llm_gpt.py
+++ apis/openai/gpt/llm_gpt.py
@@ -34,7 +34,7 @@ class GPT3Model(metaclass=ABCMeta):
         return self._use_model
 
     @abstractmethod
-    def create_completion(self, messages: Union[str, dict[str, any]]): pass
+    def create_completion(self, prompt: Union[str, dict[str, any]]) -> str: pass
 
 
 class GPT3Completion(GPT3Model):
@@ -44,11 +44,11 @@ class GPT3Completion(GPT3Model):
         super().__init__(model, supported_models, max_tokens, temperature, top_p)
         self.supported_models = self.COMPLETION_SUPPORTED_MODELS
 
-    def create_completion(self, messages: Union[str, dict[str, any]]):
+    def create_completion(self, prompt: Union[str, dict[str, any]]) -> str:
         try:
             completion = openai.Completion.create(
                 model=self.use_model,
-                prompt=messages,
+                prompt=prompt,
                 max_tokens=self.max_tokens,
                 temperature=self.temperature,
                 top_p=self.top_p
@@ -81,8 +81,23 @@ class GPT3ChatCompletion(GPT3Model):
         self.supported_models = self.CHAT_COMPLETION_SUPPORTED_MODELS
         self.templates = [i for i in self.get_templates()]
 
-    def create_completion(self, messages: Union[str, dict[str, any]]):
-        pass
+    def create_completion(self, prompt: Union[str, dict[str, any]]) -> str:
+        try:
+            self.templates.append({"role": "user", "content": prompt})
+            completion = openai.ChatCompletion.create(
+                model=self.use_model,
+                messages=self.templates,
+                max_tokens=self.max_tokens,
+                temperature=self.temperature,
+                top_p=self.top_p
+            )
+            response_content = completion.choices[0].message.content.replace('\n', '')
+
+            logger_output(level='info', message='応答メッセージの生成に成功しました', output_filename='openai_access')
+            return response_content
+
+        except:
+            raise
 
     def get_templates(self) -> list[dict[str, str]]:
         with open('apis/openai/gpt/templates/chat_template.csv') as csv_file:
@@ -90,45 +105,4 @@ class GPT3ChatCompletion(GPT3Model):
 
             return [template for template in templates]
 
-# class GPT3ChatCompletion:
-#     def __init__(self, model: str = '', prompt: str = '', max_tokens=9, temperature=0) -> None:
-#         self.model = GPT3Model(model=model)
-#         self.prompt = prompt
-#         self.max_tokens = max_tokens
-#         self.temperature = temperature
-
-#     def create_completion(self):
-#         content = """
-#             魔理沙:やや強気で物知りな女性。文法上違和感のない限りかならず「だぜ」を語尾につけてしゃべる。小学校2年生でも理解できるように説明してくれる。
-#             """
-#         try:
-#             completion = openai.ChatCompletion.create(
-#                 model=self.model.use_model,
-#                 messages=[
-#                     {"role": "system",
-#                         "content": f"あなたは次のような人物になりきって回答をしてください\n\n{content}"},
-#                     {"role": "user", "content": "ねぇ魔理沙、最近よくChatGPTって聞くけど、何だろう？"},
-#                     {"role": "assistant",
-#                         "content": "あぁ、ChatGPTか。それはAI（人工知能）の一種で、すごく賢いコンピューターだぜ。"},
-#                     {"role": "user", "content": self.prompt}
-#                 ]
-#             )
-#             response_msg = self._create_question_with_response_content(
-#                 completion)
-#             success_msg = f"""
-#             メッセージの生成が完了しました。
-#             内容:
-
-#             {response_msg}
-#             """
-#             logger_output(file_output='openai_access',
-#                           level='info', message=success_msg)
-#             return response_msg
-
-#         except Exception:
-#             logger_output(file_output='openai_access', level='error', message=f'chatgpt apiの処理中に問題が発生しました')
-#             raise
-
-#     def _create_question_with_response_content(self, msg_content: str) -> str:
-#         return msg_content.choices[0].message.content.replace('\n', '')
 
--- request_test.py
+++
@@ -1,7 +0,0 @@
-from apis.openai.gpt.llm_gpt import GPT3ChatCompletion
-
-if __name__ == '__main__':
-    chat = GPT3ChatCompletion(
-        model='gpt-3.5-turbo'
-    )
-    print(chat.templates)

7c214aa9cfa9b4653beab10f288c016efe6ed49b
onikarubi <printuserselect@gmail.com>
2023-05-22 15:44:37 +0900

    Googleドライブにアップロード及びダウンロードを行えるツールを追加した

--- tools/gcp/google_drive_tool.py
+++ tools/gcp/google_drive_tool.py
@@ -1,5 +1,5 @@
 from pydrive2.auth import GoogleAuth
-from pydrive2.drive import GoogleDrive
+from pydrive2.drive import GoogleDrive, GoogleDriveFile
 import os
 
 class GoogleDriverAssist:
@@ -19,66 +19,75 @@ class GoogleDriverAssist:
 
 
 class GoogleDriveLoader:
-    def __init__(self, target_filename: str, folder_id: str = None) -> None:
+    def __init__(self, target_filename: str, file_id: str = None) -> None:
         self._target_filename = target_filename
-        self._folder_id = folder_id
+        self._file_id = file_id
         self._g_auth = GoogleAuth()
 
     @property
-    def target_filename(self):
+    def target_filename(self) -> str:
         return self._target_filename
 
     @property
-    def folder_id(self):
-        return self._folder_id
+    def file_id(self) -> str:
+        return self._file_id
 
     @property
-    def g_auth(self):
+    def g_auth(self) -> GoogleAuth:
         return self._g_auth
 
     def _initialize_drive(self) -> GoogleDrive:
-        self._g_auth.CommandLineAuth()
+        self.g_auth.CommandLineAuth()
         return GoogleDrive(self._g_auth)
+
+    def _get_google_drive_file(self, file_id: str = '') -> GoogleDriveFile:
+        drive = self._initialize_drive()
+        if not file_id:
+            return drive.CreateFile()
+
+        return drive.CreateFile({'id': file_id})
 
 
 class GoogleDriveUploader(GoogleDriveLoader):
-    def __init__(self, target_filename: str, folder_id: str = None) -> None:
-        super().__init__(target_filename, folder_id)
-        self.drive = self._initialize_drive()
-
-        if self.folder_id:
-            self.target = self.drive.CreateFile({"parents": [{'id': folder_id}]})
-
-        else:
-            self.target = self.drive.CreateFile()
+    def __init__(self, target_filename: str, file_id: str = None) -> None:
+        super().__init__(target_filename, file_id)
 
     @GoogleDriverAssist.service_transaction
     def upload(self) -> None:
-        self.target.SetContentFile(self.target_filename)
-        self.target['title'] = os.path.basename(self.target_filename)
-        self.target.Upload()
+        target_file = self._get_google_drive_file(self.file_id)
+        target_file.SetContentFile(self.target_filename)
+        target_file['title'] = os.path.basename(self.target_filename)
+        target_file.Upload()
         print(self.target)
 
 
 class GoogleDriveDownloader(GoogleDriveLoader):
-    def __init__(self, target_filename: str, folder_id: str = None) -> None:
-        super().__init__(target_filename, folder_id)
+    def __init__(self, target_filename: str, file_id: str = None) -> None:
+        super().__init__(target_filename, file_id)
 
-    def download(self): pass
+    @GoogleDriverAssist.service_transaction
+    def download(self):
+        target_file = self._get_google_drive_file(file_id=self.file_id)
+        target_file.GetContentFile(self.target_filename)
 
 
 class GoogleDriveService(GoogleDriveLoader):
-    def __init__(self, target_filename: str, folder_id: str = None) -> None:
-        super().__init__(target_filename, folder_id)
+    def __init__(self, target_filename: str, file_id: str = None) -> None:
+        super().__init__(target_filename, file_id)
 
     def upload(self):
         uploader = GoogleDriveUploader(
             target_filename=self.target_filename,
-            folder_id=self.folder_id
+            file_id=self.file_id
         )
         uploader.upload()
 
     def download(self):
-        pass
+        downloader = GoogleDriveDownloader(
+            target_filename=self.target_filename,
+            file_id=self.file_id
+        )
+
+        downloader.download()
 
 
--- tools/tools.py
+++ tools/tools.py
@@ -2,9 +2,11 @@ from .gcp.google_drive_tool import GoogleDriveService
 
 def chat_template_uploader():
     drive = GoogleDriveService(
-        filename='apis/openai/gpt/templates/hoge.js',
-        folder_id='1xvS8HPN7XrAy2xWlTec1rTM2S1up-V9w'
+        target_filename='apis/openai/gpt/templates/hoge.js',
+        file_id='1swe1X7Ea7axmhopEf_XzCoxTcNCJNiPZ'
     )
 
-    drive.upload()
+    drive.download()
+
+
 
